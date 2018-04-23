7dd557007cc34de35f56f3f00a3bc52611a870f0
picky123 <38593029+picky123@users.noreply.github.com>
2018-04-23 22:23:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const token = 'NDM3NjUwNTA4NTk3OTUyNTEy.Db5JUQ.IFzvqO3BS-9yt4adDirLbRoWmbM';
+const token = 'NDM3NjUwNTA4NTk3OTUyNTEy.Db-_Xg.8RgC8BYxoW-VghxGoqkloSvmcNI';
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
@@ -10,13 +10,13 @@ client.on("ready", () => {
   client.user.setGame(`in ${client.guilds.size} servere | -help`);
 });
 
-
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'welcome');
   if (!channel) return;
   channel.send(`:hugging: Bun venit în comunitatea **WHART**, ${member}!
 :white_small_square: Ținând cont că ești o persoană nou venită pe serverul nostru, și dorim să te simti cat mai bine. 
-Ne para rau ca nu avem cel mai bun bot sau cel mai bun staff dar o sa ne dezvoltam pe parcurs. Terog uitate pe **index** o sa te ajute destul de mult`);
+Ne para rau ca nu avem cel mai bun bot sau cel mai bun staff dar o sa ne dezvoltam pe parcurs. 
+Te rog uita-te pe **index** o sa te ajute destul de mult`);
 });
 
-client.login('NDM3NjUwNTA4NTk3OTUyNTEy.Db5JUQ.IFzvqO3BS-9yt4adDirLbRoWmbM';
+client.login('NDM3NjUwNTA4NTk3OTUyNTEy.Db-_Xg.8RgC8BYxoW-VghxGoqkloSvmcNI';
